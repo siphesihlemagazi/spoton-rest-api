4ed4622dca77ca40d1b6ca06caf73ebb9e507dca
siphesihlemagazi <siphemagazi@outlook.com>
2022-05-28 11:59:16 +0200

    Spoton api using class based views

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('material/', views.MaterialAPIView.as_view()),
+    path('materials/', views.MaterialAPIView.as_view()),
     path('material/detail/<str:id>/', views.MaterialDetails.as_view()),
 ]
