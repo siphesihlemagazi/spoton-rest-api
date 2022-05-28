8019a4ca8406b1453f31186e845494f81dd4e95c
siphesihlemagazi <siphemagazi@outlook.com>
2022-05-28 12:23:26 +0200

    spoton api using generic views

--- api/views.py
+++ api/views.py
@@ -1,53 +1,13 @@
-from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render
 from api.models import Material
 from api.serializers import MaterialSerializer
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 
-class MaterialAPIView(APIView):
-
-    def get(self, request):
-        materials = Material.objects.all()
-        serializer = MaterialSerializer(materials, many=True)
-        return Response(serializer.data)
+class MaterialList(generics.ListCreateAPIView):
+    queryset = Material.objects.all()
+    serializer_class = MaterialSerializer
 
 
-    def post(self, request):
-        serializer = MaterialSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class MaterialDetails(APIView):
-
-    def get_object(self, id):
-        try:
-            return Material.objects.get(id=id)
-
-        except Material.DoesNotExist:
-            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-    def get(self, request, id):
-        material = self.get_object(id)
-        serializer = MaterialSerializer(material)
-        return Response(serializer.data)
-
-    def put(self, request, id):
-        material = self.get_object(id)
-        serializer = MaterialSerializer(material, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, id):
-        material = self.get_object(id)
-        material.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class MaterialDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Material.objects.all()
+    serializer_class = MaterialSerializer
